4d0ddf2cdd51701df2973e4be4ad00a2ed1882f8
BRIANNA KINCART <briannakincart@gmail.com>
2017-04-18 17:13:55 -0400

    adding user auth test file

---
+++ spec/features/01_user_signs_into_site_spec.rb
@@ -0,0 +1,124 @@
+require 'spec_helper'
+
+feature "Sign up", %Q{
+  As a prospective user
+  I want to create an account
+  So that I can post venues and review them
+} do
+
+  scenario "Specifying valid and required information" do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'First Name', with: 'Bob'
+    fill_in 'Last Name', with: 'Billbob'
+    fill_in 'Password' , with: 'password123'
+    fill_in 'Password Confirmation' , with: 'password123'
+    fill_in 'Email', with: 'bob.billbob@gmail.com'
+    fill_in 'Avatar URL', with: 'http://t.fod4.com/t/e0c7a42727/c640x360_38.jpg'
+    click_button 'Sign Up'
+
+    expect(page).to have_content "You're now signed in as Bob"
+    expect(page).not_to have_content "Sign Up"
+    expect(page).not_to have_content "Sign In"
+    expect(page).to have_content "Sign Out"
+  end
+
+  scenario "User omits required information" do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'First Name', with: ''
+    fill_in 'Last Name', with: ''
+    fill_in 'Password' , with: ''
+    fill_in 'Password Confirmation' , with: ''
+    fill_in 'Email', with: ''
+    fill_in 'Avatar URL', with: ''
+    click_button 'Sign Up'
+
+    expect(page).to have_content "First Name can't be blank"
+    expect(page).to have_content "Last Name can't be blank"
+    expect(page).to have_content "Password can't be blank"
+    expect(page).to have_content "Email can't be blank"
+  end
+
+
+  scenario "User gives invalid information" do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'First Name', with: 'Bob'
+    fill_in 'Last Name', with: 'Billbob'
+    fill_in 'Password' , with: '123'
+    fill_in 'Password Confirmation' , with: '321'
+    fill_in 'Email', with: 'bob.billbob@gmail'
+    fill_in 'Avatar URL', with: ''
+    click_button 'Sign Up'
+
+    expect(page).to have_content "Password is too short (minimum is 7 characters)"
+    expect(page).to have_content "Password doesn't match confirmation"
+    expect(page).to have_content "Email is not a valid email address"
+  end
+end
+
+feature "sign in", %Q{
+  As an unauthenticated user
+  I want to sign in
+  So that I can post venues and review them
+} do
+
+  scenario "user signs in successfully"
+    bob = User.new(first_name: 'Bob', last_name: 'Billbob', password: 'password123', email: 'bob.billbob@gmail.com')
+
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: 'bob.billbob@gmail.com'
+    fill_in 'Password' , with: 'password123'
+    click_link 'Sign In'
+
+    expect(page).to have_content "You're now signed in as Bob"
+    expect(page).not_to have_content "Sign Up"
+    expect(page).not_to have_content "Sign In"
+    expect(page).to have_content "Sign Out"
+  end
+
+  scenario "user gives email without an account"
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: 'brianna.billbob@gmail.com'
+    fill_in 'Password' , with: 'password123'
+    click_link 'Sign In'
+
+    expect(page).to have_content "We could not find your email! Please reenter or create an account."
+  end
+
+  scenario "user enters incorrect password"
+    bob = User.new(first_name: 'Bob', last_name: 'Billbob', password: 'password123', email: 'bob.billbob@gmail.com')
+
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: 'bob.billbob@gmail.com'
+    fill_in 'Password' , with: 'password12'
+    click_link 'Sign In'
+
+    expect(page).to have_content "Your password is incorrect."
+  end
+end
+
+feature "sign out", %Q{
+  As an authenticated user
+  I want to sign out
+  So that no one else can post venues or reviews on my behalf
+} do
+
+  scenario "successful sign out" do
+    bob = User.new(first_name: 'Bob', last_name: 'Billbob', password: 'password123', email: 'bob.billbob@gmail.com')
+
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: 'bob.billbob@gmail.com'
+    fill_in 'Password' , with: 'password123'
+    click_link 'Sign In'
+
+    click_link "Sign Out"
+
+    expect(page).to have_content "You have been signed out"
+  end
+end
