141e90aa3955cc7580b0802cb9f651cc4e64365b
BRIANNA KINCART <briannakincart@gmail.com>
2017-04-19 16:32:46 -0400

    More Hound updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar_url])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,
+      :avatar_url])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, email: true, uniqueness: { message: "An account already exists for that email address!"}
+  validates :email, presence: true, email: true,
+    uniqueness: { message: "An account already exists for that email address!" }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 end
