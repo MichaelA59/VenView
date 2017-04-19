08e84d4c6825bcc3d2ff7e1f08183e6888d768ea
BRIANNA KINCART <briannakincart@gmail.com>
2017-04-19 11:11:58 -0400

    finalized and working auth tests with Devise installed

--- spec/features/01_user_signs_into_site_spec.rb
+++ spec/features/01_user_signs_into_site_spec.rb
@@ -1,10 +1,9 @@
-require 'spec_helper'
+require 'rails_helper'
 
-feature "Sign up", %Q{
-  As a prospective user
-  I want to create an account
-  So that I can post venues and review them
-} do
+feature "Sign up" do
+  # As a prospective user
+  # I want to create an account
+  # So that I can post venues and review them
 
   scenario "Specifying valid and required information" do
     visit root_path
@@ -26,12 +25,6 @@ feature "Sign up", %Q{
   scenario "User omits required information" do
     visit root_path
     click_link 'Sign Up'
-    fill_in 'First Name', with: ''
-    fill_in 'Last Name', with: ''
-    fill_in 'Password' , with: ''
-    fill_in 'Password Confirmation' , with: ''
-    fill_in 'Email', with: ''
-    fill_in 'Avatar URL', with: ''
     click_button 'Sign Up'
 
     expect(page).to have_content "First Name can't be blank"
@@ -58,13 +51,12 @@ feature "Sign up", %Q{
   end
 end
 
-feature "sign in", %Q{
-  As an unauthenticated user
-  I want to sign in
-  So that I can post venues and review them
-} do
+feature "sign in" do
+  # As an unauthenticated user
+  # I want to sign in
+  # So that I can post venues and review them
 
-  scenario "user signs in successfully"
+  scenario "user signs in successfully" do
     bob = User.new(first_name: 'Bob', last_name: 'Billbob', password: 'password123', email: 'bob.billbob@gmail.com')
 
     visit root_path
@@ -79,7 +71,7 @@ feature "sign in", %Q{
     expect(page).to have_content "Sign Out"
   end
 
-  scenario "user gives email without an account"
+  scenario "user gives email without an account" do
     visit root_path
     click_link 'Sign In'
     fill_in 'Email', with: 'brianna.billbob@gmail.com'
@@ -89,7 +81,7 @@ feature "sign in", %Q{
     expect(page).to have_content "We could not find your email! Please reenter or create an account."
   end
 
-  scenario "user enters incorrect password"
+  scenario "user enters incorrect password" do
     bob = User.new(first_name: 'Bob', last_name: 'Billbob', password: 'password123', email: 'bob.billbob@gmail.com')
 
     visit root_path
@@ -102,11 +94,10 @@ feature "sign in", %Q{
   end
 end
 
-feature "sign out", %Q{
-  As an authenticated user
-  I want to sign out
-  So that no one else can post venues or reviews on my behalf
-} do
+feature "sign out" do
+  # As an authenticated user
+  # I want to sign out
+  # So that no one else can post venues or reviews on my behalf
 
   scenario "successful sign out" do
     bob = User.new(first_name: 'Bob', last_name: 'Billbob', password: 'password123', email: 'bob.billbob@gmail.com')
