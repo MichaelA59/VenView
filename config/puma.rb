ac2d1ec0215ff0a6ace62be49c0c3d992a462788
Keith Hubert <keith.hubert@gmail.com>
2017-04-20 16:07:38 -0400

    Updated Puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,13 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RACK_ENV'] || 'development'
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
