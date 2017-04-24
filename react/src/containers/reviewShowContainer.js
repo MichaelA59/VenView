f8af0ab518c551f1aead8a069a11bf327559c87c
Michael Adamski <mvadamski@gmail.com>
2017-04-24 14:41:39 -0400

    added react feed to homepage and restyled venue index page

---
+++ react/src/containers/reviewShowContainer.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import ReviewTile from '../components/reviewTile'
+
+class ReviewShowContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('api/v1/reviewapi')
+    .then(reviewResponse => reviewResponse.json())
+    .then(usableReviewData => {
+      this.setState({ reviews: usableReviewData })
+    })
+  }
+
+  render() {
+    let reviews = this.state.reviews.map (review => {
+      return(
+        <ReviewTile
+          key={review.id}
+          id={review.id}
+          title={review.title}
+          venue_name = {review.venue_name}
+          venue_id = {review.venue_id}
+        />
+      )
+    })
+
+    return (
+      <div className="row">
+        <div className="small-10 medium-4 columns">
+          <h3> Recent Reviews </h3>
+          <hr />
+          {reviews}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewShowContainer;
