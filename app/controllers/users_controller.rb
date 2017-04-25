400ce91b7c1c97a99886c0845979216e87eb2fb2
Christine <christinesohn01@gmail.com>
2017-04-25 11:37:33 -0400

    Admin responsibilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authorize_user, only: [:edit, :update, :show]
+  before_action :authorize_admin, only: [:index]
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews.order(created_at: :desc)
@@ -20,6 +26,18 @@ class UsersController < ApplicationController
       @user.email = params[:user][:email]
       @user.avatar_url = params[:user][:avatar_url]
       render :edit
+    end
+  end
+
+  def authorize_user
+    if !user_signed_in?
+      raise ActionController::RoutingError.new("NotFound")
+    end
+  end
+
+  def authorize_admin
+    if !user_signed_in? || !current_user.admin?
+      raise ActionController::RoutingError.new("Not Found")
     end
   end
 end
