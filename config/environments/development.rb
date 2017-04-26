f8a607a3acf2a7f2d21fdf403f161abde3be90e6
Keith Hubert <keith.hubert@gmail.com>
2017-04-26 14:23:51 -0400

    addressed hound errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,20 +28,19 @@ Rails.application.configure do
 
   # adding mailer functionality
   config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default_url_options = { :host => 'localhost' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 
-    # config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
+  # config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
     user_name:            'venview.test@gmail.com',
     password:             'launchpass',
     authentication:       'plain'
-
-     }
+  }
 
 
   # Print deprecation notices to the Rails logger.
