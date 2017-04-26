cd67d3023d8e40788967d27c161e8176611c3a5a
Keith Hubert <keith.hubert@gmail.com>
2017-04-26 11:42:03 -0400

    fixed hound violations

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,12 +3,12 @@ class VenuesController < ApplicationController
   before_action :authorize_admin, only: [:edit, :update, :destroy]
 
   def index
-  @venues = Venue.order('name ASC')
-    if params[:search]
-      @venues = Venue.search(params[:search]).order("created_at DESC")
-    else
-      @venues = Venue.all.order("created_at DESC")
-    end
+    @venues = Venue.order('name ASC')
+      if params[:search]
+        @venues = Venue.search(params[:search]).order("created_at DESC")
+      else
+        @venues = Venue.all.order("created_at DESC")
+      end
   end
 
   def show
