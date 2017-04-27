b8980a8637cdffffa007f7833287dd103cafc476
Liz Chhouk <kisaras.playground@gmail.com>
2017-04-27 09:51:27 -0400

    add DateTime

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@example.com" }
@@ -5,5 +7,6 @@ FactoryGirl.define do
     last_name 'Billbob'
     password 'password123'
     password_confirmation 'password123'
+    confirmed_at DateTime.now
   end
 end
