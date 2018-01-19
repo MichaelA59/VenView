99eb8156ee51a86173f541e273530c54d4ea7842
Brianna <briannakincart@gmail.com>
2018-01-19 15:46:24 -0500

    Changes to all of my other controllers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,23 +4,6 @@ class VotesController < ApplicationController
     redirect_to venue_path(new_vote.review.venue)
   end
 
-  def update
-    this_vote = Vote.find(params[:id])
-    this_vote.up_or_down = if this_vote.up_or_down === "upvote"
-                             "downvote"
-                           else # otherwise, this_vote.up_or_down === "downvote"
-                             "upvote"
-                           end
-    this_vote.save
-    redirect_to venue_path(this_vote.review.venue)
-  end
-
-  def destroy
-    this_vote = Vote.find(params[:id])
-    this_vote.destroy
-
-    redirect_to venue_path(this_vote.review.venue)
-  end
 
   private
 
